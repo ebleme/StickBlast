50633f03b261ed19bed5563829c98f60b7739d2d
maebleme <maebleme@gmail.com>
2025-02-06 13:15:53 +0300

    Added Grid system and Scene Desing

---
+++ Assets/Scripts/MyTile.cs
@@ -0,0 +1,17 @@
+using MyGrid.Code;
+using UnityEngine;
+
+public class MyTile : TileController
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
